82eae6990c612106674f0b63d5ba1f410abd2098
fawas <fawazmusty247@gmail.com>
2023-04-16 22:49:08 +0100

    commit

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 
 const app =express();
-const main = require('./db.js');
-const iwe = require ('./models/iwe.js')
+
 const dotenv = require ('dotenv')
  const ejs = require('ejs');
 const { urlencoded } = require('express');
 dotenv.config();
-
-main().catch(err => console.log(err));
+const prisma = require('./db/index')
 const PORT = process.env.PORT||3000;
 
 app.use(express.urlencoded({extended:true}))
@@ -19,56 +17,93 @@ app.use(express.static('public'))
 //static pages
 
 //add note
-app.get('/iwe/addNote',(req,res) =>{
+app.get('/addNote',(req,res) =>{
     res.render('addNote',{pageTitle:'Add note',})
     })
-    //update note
-    app.get('/iwe/update/:id',(req,res) =>{
-        res.render('updateNote',{pageTitle:'Update note',})
-        })
+    
 
 //Endpoints
 //get all notes
 app.get('/', async( req,res) => {
- const allNotes = await iwe.find()
+ const allNotes =  await prisma.notes.findMany();
 res.render('index',{pageTitle:'Iwè', allNotes})
 })
 
+
 //get one note
 app.get('/iwe/:id', async (req,res) =>{
-const id = req.params.id;
-   const oneNote = await iwe.findById(id)
-    res.render('oneNote',{pageTitle:'One note', oneNote})
-    })
+const id = parseInt(req.params.id);
+if (!Number.isInteger(id)) {
+  return res.status(400).send('Invalid id parameter');
+}
+   const oneNote = await prisma.notes.findUnique({
+    where: { 
+      id:id
+    },
+  });
+  res.render('oneNote',{pageTitle:'One note', oneNote})
+    });
+
 
     // delete note
     app.delete('/iwe/:id', async (req,res) =>{
-        const id = req.params.id;
-           const oneNote = await iwe.findByIdAndDelete(id)
+      const id = parseInt(req.params.id);
+      if (!Number.isInteger(id)) {
+        return res.status(400).send('Invalid id parameter');
+      }
+      try {
+        const oneNote =  await prisma.notes.delete({
+           where: { 
+            id: id
+          } 
+        });
+        res.status(200).json ({message : 'Note has been deleted' })  
+      } 
+      catch (error) {
+        res.status(400).json({error:'Note not deleted'})  
+      }
+        
             })
 
+
 //update Note
-app.put('/iwe/update/:id', async (req,res) => {
-    const id = req.params.id
-   const updateInfo = {
-       title:req.body.title,
-        content:req.body.content
-    }   
-const updReq = await iwe.findByIdAndUpdate(id, updateInfo);
+app.put('/update/:id', async (req,res) => {
+  const id = parseInt(req.params.id);
+  
+
+  if (!Number.isInteger(id)) {
+    return res.status(400).send('Invalid id parameter');
+  }
+console.log(id)
+    const { title, content } = req.body;
+    try{
+   const updReq  = await prisma.notes.update({
+      where: { id: id},
+      data: { title, content},
+    });
+    res.status(200).json({ message :"update successful"})
+  }
+  catch(error){
+  res.status(400).json({error:"update unsuccessful"})
+   }       
+    
 })
 
-//addNote
-app.post('/iwe/addNote', async (req,res) =>{
-    const newNote ={
-        title:req.body.title,
-        content:req.body.content
-    }
-    const addNote = await iwe.create(newNote)
-   
-         res.redirect('/')   
-    
-    })
 
+
+//update note
+app.get('/update/:id',(req,res) =>{
+  res.render('updateNote',{pageTitle:'Update note', })
+  })
+
+//addNote
+app.post('/addNote', async (req,res) =>{
+    const {title, content} = req.body;
+    const addNote = await prisma.notes.create({
+      data: { title, content},
+    });
+         res.redirect('/')   
+    })
 
 
 app.listen(PORT,() =>{
